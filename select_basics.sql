be23e3d32c8894f98e57302bdb86389320636c52
rumbie <rumbiemudzie@gmail.com>
2020-08-18 11:00:35 +0200

    Solve the basic select queries

---
+++ select_basics.sql
@@ -0,0 +1,12 @@
+SELECT population FROM world
+  WHERE name = 'Germany';
+
+
+SELECT name, population FROM world
+  WHERE name IN ('sweden', 'Norway', 'Denmark');
+
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000;
+
+
